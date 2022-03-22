9920b19ea96960dd95db901695b5ca47d7dfc5c1
Shaun Tornilla <70738856+ShaunTornilla@users.noreply.github.com>
2022-03-22 15:42:14 -0500

    Login and User Creation

--- API/api.py
+++ API/api.py
@@ -7,13 +7,14 @@ import time
 import pyodbc
 import requests
 import json
-import hashlib
+from passlib.hash import pbkdf2_sha256
 from itsdangerous import URLSafeTimedSerializer, TimestampSigner, SignatureExpired, BadSignature, BadData
 import os
 import logging
 import base64
 import jwt
 import pymongo
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
@@ -25,7 +26,7 @@ urlWebsite = "http://127.0.0.1:5001"
 #client = pymongo.MongoClient("mongodb+srv://ShaunTornilla:cJgxXnBgwLDYKpPy@bradleycapstoneproject.c4bea.mongodb.net/test")
 client = pymongo.MongoClient("mongodb+srv://bob:bob@bradleyschedulerapplica.s3n3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 
-db = client.application_database
+application_db = client.application_database
 
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 CLIENT_SALT = os.environ['CLIENT_SALT']
@@ -42,7 +43,7 @@ class PasswordCheck(Resource):
 
         # Hashes the password grabbed from user input
         inputted_password = request.headers.get("Password")
-        hashed_password = hashlib.sha256((CLIENT_SALT + inputted_password).encode('utf-8')).hexdigest()
+        #hashed_password = hashlib.sha256((CLIENT_SALT + inputted_password).encode('utf-8')).hexdigest()
         
         # print("\n\n\n", hashed_password, "\n\n\n")
 
@@ -96,40 +97,41 @@ class GetUserIDFromEmail(Resource):
 
         # Searches through database with given parameters and returns all data found with that parameter (email_address)
         # Ideally it should just be one element found or we gonna have problems lol
-        query = db["employees"].find({"Email": email_address})
+        query = application_db["employees"].find({"Email": email_address})
 
+        # If nothing exists in the database,
+        if query.count() == 0:
+            return response_error("400", "No account found."), 404
 
-        # Grabs employee id for the return
-        for data in query:
-            
-            result = parse_json(data)
-            
+        else:
 
-        print("\n\n\n", result, "\n\n\n")
+            # Grabs employee id for the return
+            for data in query:           
+                result = str(data["_id"])
 
-        response = make_response({'id_employee' : result}, 200)
-
-        return(response)
+            return make_response({'id_employee' : result}, 200)
 
 class GetUserData(Resource):
 
     def get(self, user_id):
 
-        #print("\n\n\n", user_id, "\n\n\n")
+        print("\n\n\n", user_id, "\n\n\n")
 
         # Creates query to extract data from
-        query = db["employee_info"].find({user_id})
+        query = application_db["employees"].find({"_id": ObjectId(user_id)})
 
         for data in query:
-
-            # parse_json handles issues regarding the objectID
-            result = data["_id"]["$oid"]
-
-        print("\n\n\n", result, "\n\n\n")
             
+            result = parse_json(data)
+
+        
+        print("\n\n\n", type(result), "\n\n\n")
+
         response = make_response({'employee_data' : result}, 200)
 
         return(response)
+
+##### 
 
 class CreateNewUser(Resource):
 
@@ -137,7 +139,7 @@ class CreateNewUser(Resource):
 
         ## Creates a new entry into the employee database (all other info will be included after email confirmation...hopefully lol)
 
-        user_entry = db["employees"].insert_one({
+        user_entry = application_db.employees.insert_one({
             "First Name": first_name, 
             "Last Name": last_name, 
             "Phone Number": "N/A",  
@@ -145,17 +147,44 @@ class CreateNewUser(Resource):
             "Street": "N/A",
             "City": "N/A",
             "State": "N/A",
-            "Zip Code": "N/A"})
+            "Zip Code": "N/A",
+            "Password": "N/A"})
         
         # parse_json handles issues regarding the objectID
         # Error given without parse_json: ObjectId is not JSON serializable
+
+        print("\n\n\n", user_entry, "\n\n\n")
 
         response = make_response({'employee_id' : parse_json(user_entry.inserted_id)}, 200)
         
         return(response)
 
+class UpdateUserInfo(Resource):
+
+    def post(self, userid, phonenumber, street, city, state, zipcode, password):
+
+        ## Searches the database by ObjectID to update the information of that user with the rest of the required info.
+        updated_info = application_db["employees"].update_one({"_id": ObjectId(userid)},
+
+            # $set will update the entries you put in the body (preferably elements already set by the createuser early on in the process)
+            {"$set":
+
+                ## The variables being updated
+                {
+                    "Phone Number": phonenumber,
+                    "Street": street,
+                    "City": city,
+                    "State": state,
+                    "Zip Code": zipcode,
+                    "Password": pbkdf2_sha256.using(rounds = 8000, salt_size = 10).hash(password)
+                }
+            }
+        )
+
+        response = make_response({'message' : 'User Creation Successful'}, 200)
 
 
+        return(response)
 
 
 
@@ -176,7 +205,7 @@ api.add_resource(GetUserIDFromEmail, '/getuseridfromemail/<string:email_address>
 api.add_resource(PasswordCheck, '/passwordcheck/<string:user_id>')
 api.add_resource(GetUserData, '/getuserdata/<string:user_id>')
 api.add_resource(CreateNewUser, '/createnewuser/<string:first_name>/<string:last_name>/<string:email>')
-# api.add_resource(CompleteUserCreation, '/completeusercreation/<string:token>')
+api.add_resource(UpdateUserInfo, '/updateuserinfo/<string:userid>/<string:phonenumber>/<string:street>/<string:city>/<string:state>/<string:zipcode>/<string:password>')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
